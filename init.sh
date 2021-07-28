181fe6e1eec4677c784295df67adb63ac4e0421c
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-07-28 19:38:31 +0300

    some work

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ set -x
 (rm -rf "$HOME/.boot" || true) && mkdir "$HOME/.boot" && cd "$HOME/.boot"
 
 curl --output master.zip -L "https://github.com/pg83/home/archive/refs/heads/master.zip"
-tar xf master.zip
+unzip master.zip
 
 cd home
 
