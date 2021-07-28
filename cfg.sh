244c3c05d7e5125290a4727b670c18d8a852b916
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-07-29 01:08:18 +0300

    some work

--- cfg.sh
+++ cfg.sh
@@ -3,14 +3,7 @@
 set -e
 set -x
 
-for d in .config .bin; do
-    mkdir -p "$HOME/$d" || true
-done
-
-rm -rf "$HOME/.config/mc" || true
-cp -R mc "$HOME/.config/"
-
-cp profile "$HOME/.profile"
+cd files && tar c . | (cd "$HOME" && tar xf -)
 
 for x in .bash_profile .bashrc; do
     rm "$HOME/$x" || true
