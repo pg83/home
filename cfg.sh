770f7a1ed54d39e731d50cb90a4d6c34b76839d9
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-07-28 20:36:10 +0300

    some work

--- cfg.sh
+++ cfg.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+mkdir -p "$HOME/.config" || true
+rm -rf "$HOME/.config/mc" || true
+
+cp -R mc "$HOME/.config/"
 cp .profile "$HOME"
 rm "$HOME/.bash_profile" || true
 rm "$HOME/.bashrc" || true
