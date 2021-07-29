f7047fd635f623e8d398e916452569881b422fa4
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-07-29 05:46:07 +0300

    some work

--- cfg.sh
+++ cfg.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-cd files && tar c . | (cd "$HOME" && tar xf -)
+(cd files && tar c .) | (cd "$HOME" && tar xf -)
 
 for x in .bash_profile .bashrc; do
     rm -rf "$HOME/$x" || true
